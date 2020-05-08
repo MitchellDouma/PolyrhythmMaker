f6918d6ed3f3da936b53813db22d009170dd8a6c
MitchellDouma <55207515+MitchellDouma@users.noreply.github.com>
2020-05-08 12:57:10 -0400

    Created Manager

---
+++ Polyrhythm Maker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Declaration
+    public GameObject firstPolyInput;
+    public GameObject secondPolyInput;
+
+    void Start()
+    {
+
+    }
+
+    public void GenerateGrid()
+    {
+
+    }
+}
